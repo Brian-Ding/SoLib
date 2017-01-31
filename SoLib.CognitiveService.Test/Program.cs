546353fe68b1cb736ce8d4ddf118002827cb1218
Yuanpeng Ding <Brian_Ding@outlook.com>
2017-01-31 15:33:51 +0800

    Microsoft Cognitive Service - ComputerVision

---
+++ SoLib.CognitiveService.Test/Program.cs
@@ -0,0 +1,12 @@
+﻿using SoLib.CognitiveService.Common;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        RequestManager manager = new RequestManager();
+        string result = manager.SendRequest("{key}", "https://imgsa.baidu.com/baike/c0%3Dbaike116%2C5%2C5%2C116%2C38/sign=9047a0f3af1ea8d39e2f7c56f6635b2b/c2fdfc039245d6887782a5ecadc27d1ed31b24f1.jpg"
+            , new VisualFeature[] { VisualFeature.Adult, VisualFeature.Categories, VisualFeature.Color, VisualFeature.Description, VisualFeature.Faces, VisualFeature.ImageType, VisualFeature.Tags }, string.Empty, Language.en);
+    }
+}
---
+++ SoLib.CognitiveService/Common/RequestManager.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections.Generic;
+using System.Net.Http;
+
+namespace SoLib.CognitiveService.Common
+{
+    public class RequestManager
+    {
+        public string SendRequest(string subscriptionKey, string imageUrl, VisualFeature[] visualFeatures, string details, Language language = Language.en)
+        {
+            Dictionary<string, string> parameters = new Dictionary<string, string>();
+            if (visualFeatures.Length > 0)
+            {
+                string features = string.Empty;
+                foreach (var feature in visualFeatures)
+                {
+                    features += feature.ToString() + ",";
+                }
+                features = features.TrimEnd(',');
+
+                parameters.Add("visualFeatures", features);
+            }
+            if (!string.IsNullOrEmpty(details))
+            {
+                parameters.Add("details", details);
+            }
+            parameters.Add("language", language.ToString());
+
+            string uri = "https://westus.api.cognitive.microsoft.com/vision/v1.0/analyze?";
+            foreach (var pair in parameters)
+            {
+                uri += $"{pair.Key}={pair.Value}&";
+            }
+            uri = uri.TrimEnd('&');
+
+            HttpClient httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Add("Ocp-Apim-Subscription-Key", subscriptionKey);
+
+            StringContent content = new StringContent($"{{\"url\":\"{imageUrl}\"}}");
+            content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+            //Task<HttpResponseMessage> responseMessageTask = httpClient.PostAsync(uri, content);
+            string result = httpClient.PostAsync(uri, content).Result.Content.ReadAsStringAsync().Result;
+
+            return result;
+        }
+    }
+
+    public enum VisualFeature
+    {
+        Categories,
+        Tags,
+        Description,
+        Faces,
+        ImageType,
+        Color,
+        Adult
+    }
+
+    public enum Language
+    {
+        en,
+        zh
+    }
+}
