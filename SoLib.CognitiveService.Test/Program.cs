f132f623bea8a08ed46a75838071d5397f782093
Yuanpeng Ding <Brian_Ding@outlook.com>
2017-01-31 16:42:59 +0800

    Computer Vision - AnazlyerImage

--- SoLib.CognitiveService.Test/Program.cs
+++ SoLib.CognitiveService.Test/Program.cs
@@ -6,7 +6,7 @@ class Program
     static void Main(string[] args)
     {
         Manager manager = new Manager();
-        IResult result = manager.AnalyzeImage("{key}",
+        IResult result = manager.AnalyzeImage("{subscription key}",
             "http://static.cnbetacdn.com/thumb/article/2017/0131/9a0adc9b1362043.jpg_600x600.jpg",
             //"https://imgsa.baidu.com/baike/c0%3Dbaike116%2C5%2C5%2C116%2C38/sign=9047a0f3af1ea8d39e2f7c56f6635b2b/c2fdfc039245d6887782a5ecadc27d1ed31b24f1.jpg",
             new VisualFeature[] {
