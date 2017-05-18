98da97f4ee04fe451fd34abf88f02def0da99513
Brian Ding <Brian.Ding@aspentech.com>
2017-05-18 15:21:39 +0800

    Add SoLib.Common

---
+++ SoLib.Common/Node.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+namespace SoLib.Common
+{
+    public class Node<T> : NotifyItem
+    {
+        private T _data;
+        public T Data
+        {
+            get
+            {
+                return _data;
+            }
+            private set
+            {
+                SetValue(ref _data, value);
+            }
+        }
+
+        private List<Node<T>> _children;
+        public List<Node<T>> Children
+        {
+            get
+            {
+                if (_children == null)
+                {
+                    _children = new List<Node<T>>();
+                }
+
+                return _children;
+            }
+            private set
+            {
+                SetValue(ref _children, value);
+            }
+        }
+    }
+}
