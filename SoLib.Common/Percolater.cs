e3baa406e8b908912176f097c11119c7777f9354
Yuanpeng Ding <Brian_Ding@outlook.com>
2017-06-12 22:27:15 +0800

    add percolate

---
+++ SoLib.Common/Percolater.cs
@@ -0,0 +1,42 @@
+using System;
+
+namespace SoLib.Common
+{
+    public class Percolater : WeightedUnionFinder
+    {
+        public Percolater(int[] items) : base(items)
+        {
+            for (int i = 0; i < items.Length; i++)
+            {
+                items[i] = -1;
+            }
+
+            int width = (int)Math.Sqrt(items.Length - 2);
+            for (int i = 0; i < width; i++)
+            {
+                Union(i + 1, 0);
+            }
+
+            for (int i = items.Length; i > items.Length - width; i++)
+            {
+                Union(i, items.Length - 1);
+            }
+        }
+
+        public void Reveal(int index)
+        {
+            Groups[index] = index;
+        }
+
+        public override bool IsConnected(int index1, int index2)
+        {
+            return base.IsConnected(index1, index2) && FindRoot(index1) != -1;
+        }
+
+        public bool IsPercolated()
+        {
+            return IsConnected(0, Groups.Length - 1);
+        }
+
+    }
+}
